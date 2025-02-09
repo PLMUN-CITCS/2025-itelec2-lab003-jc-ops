0130297fba810515686dd9f1317904605e85a5ee
Jocel Encio <jocelencio25@gmail.com>
2025-02-09 22:16:56 +0800

    Submitting Python Week 02 - Session 01 - Exercise 01

---
+++ exercise_01.py
@@ -0,0 +1,16 @@
+count = 10
+decimal_value = 3.14
+message = "Hello, Python!"
+is_active = True
+result = None
+Count = 15
+total_count = 20
+
+print("Integer (count):", count)
+print("Integer (Count):", Count)
+print("Integer (total_count):", total_count)
+print("Decimal:", decimal_value)
+print("Text:", message)
+print("Boolean:", is_active)
+print("None Value:", result)
+print("Sum: 8.00")
